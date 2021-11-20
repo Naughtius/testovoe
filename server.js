187e2f070fbcff356978c86ea46d6b3daf4f61d4
Nariman <islamovnariman@gmail.com>
2021-11-21 03:11:36 +0500

    add express

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const next = require('next')
+
+const app = next({ dev: false })
+const handle = app.getRequestHandler()
+
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+    const PORT = process.env.PORT || 3000
+
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
+
+    server.listen(PORT, (err) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:' + PORT)
+    })
+  })
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
